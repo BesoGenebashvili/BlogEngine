67fb9182f1c84ab05a44b3910be5502b278fda31
beso <52665934+BesoGenebashvili@users.noreply.github.com>
2020-10-01 23:43:46 +0400

    Added unit test project and few tests for ClientServices

---
+++ BlogEngine/BlogEngine.Tests/BlogEngine.ClientServices/Extensions/ObjectExtensionsTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using BlogEngine.ClientServices.Extensions;
+
+namespace BlogEngine.Tests.BlogEngine.ClientServices.Extensions
+{
+    [TestFixture]
+    public class ObjectExtensionsTests
+    {
+        [Test]
+        public void ToQueryString_ObjectIsNull_ThrowArgumentNullException()
+        {
+            Assert.That(() => ObjectExtensions.ToQueryString(null),
+                Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void ToQueryString_WhenCalled_ReturnsQueryString()
+        {
+            var obj = new { Bar = "Bar", Foo = "Foo" };
+
+            string result = ObjectExtensions.ToQueryString(obj);
+
+            Assert.That(result, Is.EqualTo("Bar=Bar&Foo=Foo").IgnoreCase);
+        }
+    }
+}
